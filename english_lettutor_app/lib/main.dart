d2b6fec62c8081abcfcc2f842c8e0a6f38737d6d
Phạm Minh Vương <minhvuongpham025@gmail.com>
2021-10-09 17:10:27 +0700

    create branch ui: new ui

--- english_lettutor_app/lib/main.dart
+++ english_lettutor_app/lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'English Lettutor App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'English Lettutor App'),
     );
   }
 }
@@ -34,7 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
     });
   }
 
@@ -47,14 +46,10 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: const <Widget>[
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+              'You have pushed the button this many times:',
+            )
           ],
         ),
       ),

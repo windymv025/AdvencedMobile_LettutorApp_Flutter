ac5d51cb2b2e166e5b3d4fb6336468926f2cc7bf
Phạm Minh Vương <minhvuongpham025@gmail.com>
2021-10-16 22:27:18 +0700

    routes Navigator

---
+++ english_lettutor_app/lib/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:english_lettutor_app/ui/screen/forgot_password/forgot_password_screen.dart';
+import 'package:english_lettutor_app/ui/screen/home/home_screen.dart';
+import 'package:english_lettutor_app/ui/screen/reset_password_screen/reset_password_screen.dart';
+import 'package:english_lettutor_app/ui/screen/sign_in/sign_in_screen.dart';
+import 'package:english_lettutor_app/ui/screen/sign_up/sign_up_screen.dart';
+import 'package:english_lettutor_app/ui/screen/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => const SplashScreen(),
+  SignInScreen.routeName: (context) => const SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
+  ResetPasswordScreen.routename: (context) => const ResetPasswordScreen(),
+  SignUpScreen.routeName: (context) => const SignUpScreen(),
+  HomeScreen.routeName: (context) => const HomeScreen(),
+};

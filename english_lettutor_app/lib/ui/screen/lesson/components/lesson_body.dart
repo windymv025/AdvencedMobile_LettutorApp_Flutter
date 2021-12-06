08305d6c2fee27a622ecade1d26329486fee256c
Phạm Minh Vương <minhvuongpham025@gmail.com>
2021-12-06 16:08:54 +0700

    update count time in lesson screen, show login in lesson screen

--- english_lettutor_app/lib/ui/screen/lesson/components/lesson_body.dart
+++ english_lettutor_app/lib/ui/screen/lesson/components/lesson_body.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:english_lettutor_app/constants/assets.dart';
 import 'package:english_lettutor_app/constants/constants.dart';
+import 'package:english_lettutor_app/models/teacher/schedule.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class LessonBody extends StatefulWidget {
   const LessonBody({Key? key}) : super(key: key);
@@ -11,8 +15,32 @@ class LessonBody extends StatefulWidget {
 
 class _LessonBodyState extends State<LessonBody> {
   @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+  }
+
+  String getUntilTime(DateTime time) {
+    final now = DateTime.now();
+    final difference = time.difference(now);
+    final days = difference.inDays;
+    final hours = difference.inHours - days * 24;
+    final minutes = difference.inMinutes - days * 24 * 60 - hours * 60;
+    final seconds = difference.inSeconds -
+        days * 24 * 60 * 60 -
+        hours * 60 * 60 -
+        minutes * 60;
+    return "$days Days $hours Hours $minutes Minutes $seconds Seconds";
+  }
+
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    Schedule schedule = ModalRoute.of(context)?.settings.arguments as Schedule;
+    String strTimeUntil = getUntilTime(schedule.time.start);
+
     return Container(
       color: Colors.black,
       child: Center(
@@ -27,13 +55,12 @@ class _LessonBodyState extends State<LessonBody> {
             ),
             Center(
               child: Container(
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      "20:00:00 until lesson start (Sat, 23 Oct 21 22:00)",
+                      "$strTimeUntil\n until lesson start (${DateFormat("HH:mm, dd - MM - yyyy").format(schedule.time.start)})",
                       textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white, fontSize: textSizeTitle),
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
                       overflow: TextOverflow.clip,
                     ),
                   ),
--- english_lettutor_app/lib/ui/screen/lesson/lesson_screen.dart
+++ english_lettutor_app/lib/ui/screen/lesson/lesson_screen.dart
@@ -1,4 +1,8 @@
+import 'package:english_lettutor_app/constants/assets.dart';
+import 'package:english_lettutor_app/data/provider/home_state.dart';
+import 'package:english_lettutor_app/ui/screen/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'components/lesson_body.dart';
 
@@ -8,8 +12,30 @@ class LessonScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: LessonBody(),
-    );
+    return Consumer<HomeState>(builder: (context, homeState, child) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              )),
+        ),
+        body: const LessonBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            homeState.pageIndex = 0;
+            Navigator.of(context).pushNamed(HomeScreen.routeName);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(Assets.assetsImagesLargeLogo),
+          ),
+          backgroundColor: Colors.white,
+        ),
+      );
+    });
   }
 }
